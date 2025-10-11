c055b5799163f97dce9b55e3e62693707487e451
CiroBurro <filobaglioni.06@proton.me>
2025-10-11 18:12:45 +0200

    listener updated

--- src/listener.rs
+++ src/listener.rs
@@ -2,6 +2,7 @@ use crate::errors::TCPError;
 use anyhow::{self, Context};
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
+use tokio::io::{stderr, stdin, stdout, BufReader, BufWriter};
 use tokio::net::TcpListener;
 
 pub async fn listen(ip: &str, port: u16) -> anyhow::Result<()> {
@@ -9,11 +10,31 @@ pub async fn listen(ip: &str, port: u16) -> anyhow::Result<()> {
 	let listener: TcpListener = TcpListener::bind(addr).await?;
 	println!("Listening...");
 
-	match listener.accept().await {
-		Ok(_s) => {
-			println!("Connected");
-			todo!()
+	loop {
+		match listener.accept().await {
+			Ok(s) => {
+				println!("Connected");
+				let stream = s.0;
+				let (reader, writer) = stream.into_split();
+				let mut socket_stdin = BufWriter::new(writer);
+				let mut socket_stdout = BufReader::new(reader);
+
+				let mut stdin = stdin();
+				let mut stdout = stdout();
+				let mut stderr = stderr();
+
+				tokio::spawn(async move {
+					tokio::io::copy(&mut stdin, &mut socket_stdin).await.expect("Failed to copy from stdin to the socket");
+				});
+
+				tokio::spawn(async move {
+					tokio::io::copy(&mut socket_stdout, &mut stdout).await.expect("Failed to copy from the socket to stdout");
+					tokio::io::copy(&mut socket_stdout, &mut stderr).await.expect("Failed to copy from the socket to stderr");
+				});
+			}
+			Err(_e) => {
+				return Err(TCPError::ConnectionFailure(addr).into());
+			}
 		}
-		Err(_e) => Err(TCPError::ConnectionFailure(addr).into())
 	}
 }
