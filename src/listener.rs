ba5a9fd5068f01d9e952462c4e6dca7ac78511b1
CiroBurro <filobaglioni.06@proton.me>
2025-10-12 19:40:21 +0200

    exit input handled

--- src/listener.rs
+++ src/listener.rs
@@ -1,9 +1,16 @@
 use crate::errors::TCPError;
 use anyhow::{self, Context};
-use std::net::{IpAddr, SocketAddr};
-use std::str::FromStr;
-use tokio::io::{stderr, stdin, stdout, BufReader, BufWriter};
-use tokio::net::TcpListener;
+use std::{
+	net::{IpAddr, SocketAddr},
+	process::exit,
+	str::FromStr,
+};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
+use tokio::{
+	io::{stderr, stdin, stdout, BufReader, BufWriter},
+	net::TcpListener,
+	sync::mpsc,
+};
 
 pub async fn listen(ip: &str, port: u16) -> anyhow::Result<()> {
 	let addr: SocketAddr = SocketAddr::new(IpAddr::from_str("0.0.0.0").with_context(|| format!("Failed to convert the string: {ip} to an IP address"))?, port);
@@ -13,17 +20,34 @@ pub async fn listen(ip: &str, port: u16) -> anyhow::Result<()> {
 	match listener.accept().await {
 		Ok(s) => {
 			println!("Connected");
+			let (tx, mut rx) = mpsc::channel::<()>(1);
 			let stream = s.0;
 			let (reader, writer) = stream.into_split();
 			let mut socket_stdin = BufWriter::new(writer);
 			let mut socket_stdout = BufReader::new(reader);
 
-			let mut stdin = stdin();
+			let stdin = stdin();
 			let mut stdout = stdout();
 			let mut stderr = stderr();
 
 			let stdin_handle = tokio::spawn(async move {
-				tokio::io::copy(&mut stdin, &mut socket_stdin).await.expect("Failed to copy from stdin to the socket");
+				let mut bufreader = BufReader::new(stdin);
+				let mut line: String = String::new();
+
+				loop {
+					line.clear();
+					let n = bufreader.read_line(&mut line).await.expect("Failed to read line from stdin (listener)");
+
+					if n == 0 { break; } // EOF
+
+					if line.trim().eq_ignore_ascii_case("exit") {
+						let _ = tx.send(()).await;
+						break;
+					}
+
+					socket_stdin.write_all(line.as_bytes()).await.context("Failed to write stdin to socket").expect("Failed to write stdin to socket");
+					socket_stdin.flush().await.context("Failed to flush socket").expect("Failed to flush socket");
+				}
 			});
 
 			let stdout_handle = tokio::spawn(async move {
@@ -31,8 +55,14 @@ pub async fn listen(ip: &str, port: u16) -> anyhow::Result<()> {
 				tokio::io::copy(&mut socket_stdout, &mut stderr).await.expect("Failed to copy from the socket to stderr");
 			});
 
-			let _ = stdin_handle.await;
-			let _ = stdout_handle.await;
+
+			tokio::select! {
+				_ = stdin_handle => {},
+				_ = stdout_handle => {},
+				_ = rx.recv() => {
+					exit(0);
+				}
+			}
 
 			Ok(())
 		}
