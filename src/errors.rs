66d05da1b89780f171fed96b868a9d5b6bd4905e
CiroBurro <filobaglioni.06@proton.me>
2025-10-14 10:57:41 +0200

    errors docs

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,12 @@
 use std::net::SocketAddr;
 use thiserror::Error;
 
+/// TCPError enum
+/// It handles errors related to the TCP connection
+///
+/// Variants:
+/// - SocketCreationFailure: Occurs when the reverse shell failes to create the socket
+/// - ConnectionFailure: Occurs when the connection between the reverse shell and the listener failes
 #[derive(Error, Debug)]
 pub enum TCPError {
 	#[error("Failed to create socket")]
@@ -10,6 +16,12 @@ pub enum TCPError {
 	ConnectionFailure(SocketAddr),
 }
 
+/// CLIError enum
+/// It handles errors related to cli arguments
+///
+/// Variants:
+/// - EmptyArgument: Occurs when a flag is specified with no content
+/// - IncompatibleArgs: Occurs when two incompatible flags are specified together
 #[derive(Error, Debug)]
 pub enum CLIError {
 	#[error("The argument: {arg} cannot be empty if its flag is specified")]
